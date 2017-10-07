6cb2152e37ebb90ecbe1c0f3d378fe4fd2b86d53
Chengcheng Shao <shaoc@indiana.edu>
2017-10-07 17:33:05 -0400

    dump all centralities

--- hnetwork/centrality.py
+++ hnetwork/centrality.py
@@ -19,14 +19,36 @@ def load_graph(fn):
                                   ecols=(2,3),
                                   )
 
-def v_betweenness(g, fn):
-    vp, ep = gt.betweenness(g)
-    vb = {g.vertex(i):vp[g.vertex(i) for i in range(g.num_vertices())]}
-    s = pd.Series(vb)
-    s.name = 'v_betweenness'
-    s.index.name = 'raw_id'
-    s = s.sort_values(ascending=False)
-    s.to_csv(fn)
+
+def centralities(g):
+    # degrees
+    # in degree
+    ki = g.degree_property_map('in')
+    # out degree
+    ko = g.degree_property_map('out')
+    # weighted in degree
+    si = g.degree_property_map('in', weight=g.ep['weight'])
+    # weighted out degree
+    so = g.degree_property_map('out', weight=g.ep['weight'])
+    # pagerank
+    pr = gt.pagerank(g)
+    # betweetnness
+    vb, eb = gt.betweenness(g)
+    # eigenvector
+    ev = gt.eigenvector(g)
+    # screen_name
+    sn = [g.vp['name'][v] for v in g.vertices()]
+    df = pd.DataFrame(dict(
+        screen_name=sn,
+        in_degree=ki.a,
+        out_degree=ko.a,
+        weighted_in_degree=si.a,
+        weighted_out_degree=so.a,
+        page_rank=pr.a,
+        betweenness=vb.a,
+        eigenvector=ev.a
+    ))
+    df.to_csv('centralities.csv')
 
 
 def v_percolate(g, ofn, vertices):
@@ -38,6 +60,3 @@ def v_percolate(g, ofn, vertices):
     plot(sizes, label='Targeting')
     plot(sizes2, lable='Random')
     plt.savefig(ofn)
-
-
-
